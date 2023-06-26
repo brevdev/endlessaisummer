cf8e174931d489d50e0fd169ce21c05fd4b12dd3
Nader Khalil <nader@brev.dev>
2023-06-26 18:09:45 -0400

    endless summer w/ joerobo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
 import celo from '@/images/celo.png'
+import roboflow from '@/images/roboflowwhite.png'
 import brev from '@/images/logo_dark_mode.png'
 import Image from 'next/image'
 
@@ -35,15 +36,26 @@ export function AltFooter() {
           unoptimized
         />
         <p className="mx-1 mt-2 text-white">• </p>
-        <a href="https://metaphor.systems" target="_blank" rel="no_referrer">
+        <Image
+          // className="absolute left-0 top-0 translate-x-[-55%] translate-y-[-10%] -scale-x-100 sm:left-1/2 sm:translate-x-[-98%] sm:translate-y-[-6%] lg:translate-x-[-106%] xl:translate-x-[-122%]"
+          src={roboflow.src}
+          className="mt-2"
+          alt=""
+          width={70}
+          height={70}
+          priority
+          unoptimized
+        />
+        {/* <a href="https://metaphor.systems" target="_blank" rel="no_referrer">
           <p className="font-ibm-plex-mono mx-1 mb-2 mt-2 text-2xl text-white">
             Metaphor
           </p>
-        </a>
+        </a> */}
         <p className="mx-1 mt-2 text-white">• </p>
         <Image
           // className="absolute left-0 top-0 translate-x-[-55%] translate-y-[-10%] -scale-x-100 sm:left-1/2 sm:translate-x-[-98%] sm:translate-y-[-6%] lg:translate-x-[-106%] xl:translate-x-[-122%]"
           src={celo.src}
+          className="mt-2"
           alt=""
           width={50}
           height={50}
@@ -53,7 +65,7 @@ export function AltFooter() {
       </div>
       <p className="text-sm leading-6 text-gray-600">
         <div className=" h-full w-full text-center text-white">
-          ENDLESS AI SUMMER ™️ • INTENSELY NON-REMOTE IN SAN FRANCISCO
+          ENDLESS AI SUMMER ™️ • HELLA IN-PERSON IN SAN FRANCISCO
         </div>
       </p>
     </div>
