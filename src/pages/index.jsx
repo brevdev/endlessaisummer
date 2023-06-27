f9b785abd508931166b98950120f21997fa47e8e
Nader Khalil <nader@brev.dev>
2023-06-26 22:44:35 -0400

    thumbnails

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,36 +7,36 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Simplest way to get a CPU dev environment on Brev.dev</title>
+        <title>Endless AI Summer</title>
         <meta
           name="description"
-          content="Get a CPU dev environment on Brev.dev"
+          content="See all the exciting AI events happening in SF this summer"
         />
-        <meta property="og:image" content="/thumbnail.jpeg" />
-        <meta property="og:image:url" content="/thumbnail.jpeg" />
-        <meta property="og:image:secure_url" content="/thumbnail.jpeg" />
-        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:image:url" content="/thumbnail.png" />
+        <meta property="og:image:secure_url" content="/thumbnail.png" />
+        <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Come build your own model" />
-        <meta name="twitter:card" content="/thumbnail.jpeg" />
-        <meta name="twitter:image" content="/thumbnail.jpeg" />
+        <meta property="og:image:alt" content="SF is ?dead" />
+        <meta name="twitter:card" content="/thumbnail.png" />
+        <meta name="twitter:image" content="/thumbnail.png" />
         <meta
           name="twitter:image:alt"
           content="A description of the image for better accessibility"
         />
-        <meta itemProp="image" content="/thumbnail.jpeg" />
+        <meta itemProp="image" content="/thumbnail.png" />
 
         <meta
           property="og:image:alt"
           content="A description of the image for better accessibility"
         />
-        <meta name="twitter:image" content="/thumbnail.jpeg" />
+        <meta name="twitter:image" content="/thumbnail.png" />
         <meta
           name="twitter:image:alt"
           content="A description of the image for better accessibility"
         />
-        <meta itemProp="image" content="/thumbnail.jpeg" />
+        <meta itemProp="image" content="/thumbnail.png" />
       </Head>
       <main className="gradient">
         <Main />
